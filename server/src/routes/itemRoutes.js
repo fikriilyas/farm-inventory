e663ec54b95b4fd4b15eb7c584ece8dd58431864
Fikri  Ilyas <fikri.ilyas@ifg-life.id>
2026-04-26 21:36:42 +0700

    fix: reorder routes to fix /batch endpoint 404

--- server/src/routes/itemRoutes.js
+++ server/src/routes/itemRoutes.js
@@ -4,9 +4,9 @@ const itemController = require('../controllers/itemController');
 const { requireAuth } = require('../middleware/authMiddleware');
 
 router.get('/', requireAuth, itemController.getAll);
-router.get('/:id', requireAuth, itemController.getById);
 router.post('/', requireAuth, itemController.create);
 router.post('/batch', requireAuth, itemController.batchAdd);
+router.get('/:id', requireAuth, itemController.getById);
 router.put('/:id', requireAuth, itemController.update);
 router.delete('/:id', requireAuth, itemController.remove);
 
